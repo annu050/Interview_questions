200414a1691be7da369c43b8b759a48376f52c4d
anusha tiwari <128884359+annu050@users.noreply.github.com>
2023-09-16 11:04:37 +0530

    Time: 17 ms (17.62%) | Memory: 13.7 MB (13.49%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool cols0=1;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        cols0=0;//if zero occurs in 1st col. only
+                    }
+                }
+            }
+        }
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=0;j<matrix[0].size();j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(cols0==0){
+           for(int i=0;i<matrix.size();i++){
+               matrix[i][0]=0;
+           } 
+        }
+       
+
+    }
+};
