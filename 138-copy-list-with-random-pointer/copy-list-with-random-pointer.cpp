8e703529b9ac1827525f35d93f60dcc1193c2786
anusha tiwari <128884359+annu050@users.noreply.github.com>
2023-10-27 21:06:02 +0530

    Time: 5 ms (72.85%) | Memory: 11.6 MB (25.96%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -16,22 +16,51 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
-       if(head==NULL){
-           return NULL;
+    Node* copyRandomList(Node* head) { 
+       if(!head){
+           return nullptr ;
        }
-       unordered_map<Node*,Node*>ans;
        Node* curr=head;
-       while(curr!=NULL){
-           ans[curr]=new Node(curr->val);
-           curr=curr->next;
+       while(curr){
+           Node* newn=new Node(curr->val);
+           newn->next=curr->next;
+           curr->next=newn;
+           curr=newn->next;
        }
        curr=head;
-       while(curr!=NULL){
-           ans[curr]->next=ans[curr->next];
-           ans[curr]->random=ans[curr->random];
-           curr=curr->next;
+       while(curr){
+           if(curr->random){
+               curr->next->random=curr->random->next;
+           }
+           curr=curr->next->next;
        }
-       return ans[head];
+       Node* oldhead=head;
+       Node* newhead=head->next;
+       Node* currold=oldhead;
+       Node* currnew=newhead;
+       while(currold){
+           currold->next=currold->next->next;
+           currnew->next=currnew->next ? currnew->next->next:nullptr;
+           currold = currold->next;
+           currnew=currnew->next; 
+       }
+       return newhead;
     }
 };
+
+// if(head==NULL){
+//            return NULL;
+//        }
+//        unordered_map<Node*,Node*>ans;
+//        Node* curr=head;
+//        while(curr!=NULL){
+//            ans[curr]=new Node(curr->val);
+//            curr=curr->next;
+//        }
+//        curr=head;
+//        while(curr!=NULL){
+//            ans[curr]->next=ans[curr->next];
+//            ans[curr]->random=ans[curr->random];
+//            curr=curr->next;
+//        }
+//        return ans[head];
