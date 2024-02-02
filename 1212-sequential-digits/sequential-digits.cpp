92c91579b37196425809aab8bbf0c5c49cbed210
anusha tiwari <128884359+annu050@users.noreply.github.com>
2024-02-02 11:39:52 +0530

    Time: 3 ms (17.15%) | Memory: 7.4 MB (9.62%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+        for(int i=1;i<9;i++){
+            int num=i;
+            int nextdigit=i+1;
+            while(num<=high && nextdigit<=9){
+                num=num*10+nextdigit;
+                if(num>=low && num<=high){
+                    ans.push_back(num);
+                }
+                nextdigit++;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
