71376e94adb9e6367c1113e15d2531be9606c452
anusha tiwari <128884359+annu050@users.noreply.github.com>
2024-02-02 12:58:51 +0530

    Time: 4 ms (76.53%) | Memory: 7.4 MB (72.25%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    bool ispalindrome(string ans){
-        string q=ans;
-        reverse(q.begin(),q.end());
-        if(q==ans){
-            return true;
-        }
-        return false;
+int ispalindrome(int s,int e,const string& c){
+    int count=0;
+    while(s>=0 && e < c.length() && c[s]==c[e]){
+        s--;
+        e++;
+        count++;
     }
-    int countSubstrings(string s) {
-        int count = 0;
-
-        for (int l = 0; l < s.size(); l++) {
-            string ans = "";
-            for (int r = l; r < s.size(); r++) {
-                ans += s[r];
-                if (ispalindrome(ans)) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
+    return count;
+}
+   int countSubstrings(string s){
+    if(s.length()==0)return 0;
+    int n=s.length();
+    int res=0;
+    for(int i=0;i<n;i++){
+        //for odd center(i.i)
+        //for even center(i,i+1)
+        res+=ispalindrome(i,i,s);
+        res+=ispalindrome(i,i+1,s);   
     }
+    return res;
+}
+
 };
+
+
+//EXPAND AROUND THE CENTERS AND EXPAND
