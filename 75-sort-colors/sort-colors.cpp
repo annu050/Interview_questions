b7c219e2e70f9c98d82f2637b773630cfdae942f
anusha tiwari <128884359+annu050@users.noreply.github.com>
2024-02-02 11:49:30 +0530

    Time: 4 ms (32.81%) | Memory: 9.8 MB (17.14%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -30,3 +30,20 @@ public:
         
     }
 };
+
+//dutch national flag
+// int low=0,mid=0,high=nums.size()-1;
+// while(mid<=high){
+//     if(nums[mid]==0){
+//         swap(nums[low],nums[mid]);
+//         mid++;
+//         low++;
+//     }
+//     else if(nums[mid]==1){
+//         mid++;
+//     }
+//     else{
+//         swap(nums[mid],nums[high]);
+//         high--;
+//     }
+// }
