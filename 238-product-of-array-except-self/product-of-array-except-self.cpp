1f892fd0bddef77b65bc27dd0aedf7c752443366
anusha tiwari <128884359+annu050@users.noreply.github.com>
2024-02-03 21:06:05 +0530

    Time: 9 ms (97.16%) | Memory: 27.5 MB (8.10%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        //doing prefix and suffix sum 
+        int n=nums.size(),prod=1;
+        vector<int> ans(n),p(n),s(n);
+        for(int i=0;i<n;i++){
+            prod*=nums[i];//cumilitive product
+            p[i]=prod;
+        }
+        prod=1;
+        for(int i=n-1;i>=0;i--){
+            prod*=nums[i];//cumilitive product
+            s[i]=prod;
+        }
+        ans[0]=s[1];
+        ans[n-1]=p[n-2];
+        for(int i=1;i<n-1;i++){
+            ans[i]=p[i-1]*s[i+1];
+        }
+        
+        return ans;
+    }
+};
