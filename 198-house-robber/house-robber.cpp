0f66741154a47786aa06bba594778ff2fd8cba82
anusha tiwari <128884359+annu050@users.noreply.github.com>
2024-02-26 11:37:32 +0530

    Time: 4 ms (31.18%) | Memory: 9 MB (64.02%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int a=0,b=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                a=max(a+nums[i],b);
+            }
+            else{
+                b=max(a,b+nums[i]);
+            }
+        }
+        return max(a,b);
+    }
+};
