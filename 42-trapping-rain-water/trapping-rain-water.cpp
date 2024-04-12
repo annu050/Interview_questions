cb4c721336664c379d57799d219b72da1bf61c92
anusha tiwari <128884359+annu050@users.noreply.github.com>
2024-04-12 09:04:13 +0530

    Time: 15 ms (21.13%) | Memory: 22.3 MB (58.72%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+//brute is min(leftmaxheight,rightmaxheight)-a[i]  water stored at each index
+//use of prefixmax and suffixmax then use the formula TC:O(N) 
     int trap(vector<int>& heights) {
-        //using two pointer approach like find the highest height from left side and right side and for sum add the max side - height[current] one
+        //using two pointer approach like find the highest height from left 
+        // side and right side and for sum add the max side - height[current] one
         // int w=0;
          int n=heights.size();
-        // int lmax=height[0];
+        // int lmax=height[0];             
         // int rmax=height[n-1];
         // int lpos=1;
         // int rpos=n-2;
@@ -27,7 +30,7 @@ public:
         //     }
         // }
         // return w;
-         int left = 0, right = n - 1, water = 0;
+    int left = 0, right = n - 1, water = 0;
  
     int maxLeft = heights[left];
     int maxRight = heights[right];
@@ -40,7 +43,8 @@ public:
             maxLeft = max(maxLeft, heights[left]);
             water += (maxLeft - heights[left]);
         }
-        else {
+        else 
+        {
             right--;
             maxRight = max(maxRight, heights[right]);
             water += (maxRight - heights[right]);
